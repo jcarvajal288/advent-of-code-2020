6e0144bfda3c6eb6a4fe526c1235cdecfcb9ec51
Max Carvajal <jcarvajal@crimson.ua.edu>
2020-12-04 18:32:26 -0600

    Solved day 1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,7 +1,34 @@
 import scala.io.BufferedSource
 
 class Day1 {
+
+  def getEntries(reportFile: BufferedSource): List[Int] = {
+    reportFile
+      .getLines()
+      .map((line: String) => {
+        line.toInt
+      })
+      .toList
+  }
+
   def solveReport(reportFile: BufferedSource): Int = {
+    val entries = getEntries(reportFile)
+    for(a <- entries; b <- entries) {
+      if ((a + b) == 2020) {
+        return a * b
+      }
+    }
+    0
+  }
+
+  def solveExtendedReport(reportFile: BufferedSource): Int = {
+    val entries = getEntries(reportFile)
+    for(a <- entries; b <- entries; c <- entries) {
+      //println(s"$a - $b - $c")
+      if ((a + b + c) == 2020) {
+        return a * b * c
+      }
+    }
     0
   }
 }
--- src/test/scala/Day1Test.scala
+++ src/test/scala/Day1Test.scala
@@ -4,11 +4,22 @@ import scala.io.Source
 
 class Day1Test extends AnyFlatSpec {
 
-  val day1 = new Day1()
-  val exampleReport = Source.fromResource("Day1ExampleInput")
+  private val day1 = new Day1()
 
   it should "solve the example problem" in {
+    val exampleReport = Source.fromResource("Day1ExampleInput")
     assert(day1.solveReport(exampleReport) == 514579)
   }
 
+  it should "solve the full problem" in {
+    val fullReport = Source.fromResource("Day1FullInput")
+    val solution = day1.solveReport(fullReport)
+    println(solution)
+  }
+
+  it should "solve the extended problem" in {
+    val fullReport = Source.fromResource("Day1FullInput")
+    val solution = day1.solveExtendedReport(fullReport)
+    println(solution)
+  }
 }
